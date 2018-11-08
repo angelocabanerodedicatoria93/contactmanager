b7fed8188a822a1ed280c02adec0b10c46e3d9af
angelocabanerodedicatoria93 <angelodedicatoria93@gmail.com>
2018-11-08 18:24:55 +0900

    Updates

---
+++ precache-manifest.78968daa39b9a91aee02c20a458caf7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "ab426280b42073ceb512",
+    "url": "/contactmanager/static/js/main.ab426280.chunk.js"
+  },
+  {
+    "revision": "5770d9e7442e26289119",
+    "url": "/contactmanager/static/js/1.5770d9e7.chunk.js"
+  },
+  {
+    "revision": "ab426280b42073ceb512",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "5770d9e7442e26289119",
+    "url": "/contactmanager/static/css/1.139caa43.chunk.css"
+  },
+  {
+    "revision": "f44bf4c2b92abf71a3b6519ca97ecb04",
+    "url": "/contactmanager/index.html"
+  }
+];
